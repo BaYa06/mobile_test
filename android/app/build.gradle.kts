e20a393f751d330258f0d0b783f3e1af886dca4f
BaYa06 <holikovbairbek06@gmail.com>
2025-04-25 17:21:16 +0600

    v1.7

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.example.schedule"
     compileSdk = flutter.compileSdkVersion.toInt()
-    ndkVersion = "25.2.9519653"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
