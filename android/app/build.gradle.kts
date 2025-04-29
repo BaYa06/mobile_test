5fc36d52d64c3d8267fdbeb92c8e1e0d2a9d41b8
BaYa06 <holikovbairbek06@gmail.com>
2025-04-29 12:01:00 +0600

    v1.8

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,11 +29,10 @@ android {
     buildTypes {
         release {
             signingConfig = signingConfigs.getByName("debug")
-            
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
         }
-        
         debug {
             signingConfig = signingConfigs.getByName("debug")
         }
